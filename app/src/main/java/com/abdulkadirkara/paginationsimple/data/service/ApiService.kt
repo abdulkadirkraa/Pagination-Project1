0173ed958de9d1fdb434bf5058410bdf5d006e9f
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-06-26 11:01:15 +0300

    add api service interface

---
+++ app/src/main/java/com/abdulkadirkara/paginationsimple/data/service/ApiService.kt
@@ -0,0 +1,14 @@
+package com.abdulkadirkara.paginationsimple.data.service
+
+import com.abdulkadirkara.paginationsimple.data.model.UserResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("api/")
+    suspend fun getUsers(
+        @Query("page") page: Int,
+        @Query("results") results: Int
+    ): UserResponse
+
+}
