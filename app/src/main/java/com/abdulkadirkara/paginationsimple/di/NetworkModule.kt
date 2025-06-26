94092c0f3d771f1f8b5baf42efc446f6f37bd052
abdulkadirkraa <karaabdulkadir498@gmail.com>
2025-06-26 11:02:18 +0300

    add hilt network module and constants base url

---
+++ app/src/main/java/com/abdulkadirkara/paginationsimple/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.abdulkadirkara.paginationsimple.di
+
+import com.abdulkadirkara.paginationsimple.data.service.ApiService
+import com.abdulkadirkara.paginationsimple.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import java.util.concurrent.TimeUnit.SECONDS
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val CONNECT_TIMEOUT = 20L
+    private const val READ_TIMEOUT = 60L
+    private const val WRITE_TIMEOUT = 120L
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(CONNECT_TIMEOUT, SECONDS)
+            .readTimeout(READ_TIMEOUT, SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun getRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun getApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/abdulkadirkara/paginationsimple/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.abdulkadirkara.paginationsimple.util
+
+object Constants {
+    const val BASE_URL = "https://randomuser.me/"
+}
